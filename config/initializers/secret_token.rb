894c60f9e7143224a3978bbbe5b1a419b223e499
Godfrey Chan <godfreykfc@gmail.com>
2012-12-05 01:14:40 -0800

    Initial commit (base Rails 3.2.9 app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarbonFiberHackers::Application.config.secret_token = 'b7d809daf2c64b1c75d0a8f7ec6eb5b87e3faef9602a64a4f3ae57201e9adab34a244c05af91b0645c3ef1f63d0a62cfdb3ca627c8543b15670875359260e346'
